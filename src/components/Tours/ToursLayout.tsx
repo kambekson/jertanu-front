748ec1b4b07734b4fb9afbe3e325bba7d97e826d
kkenz <k.28.06.2002@gmail.com>
2025-04-17 15:04:47 +0500

    feat: refactor Tours components
    This commit introduces new components for the Tours section, including ToursHero, ToursList, ToursFilters, and ToursLayout. The

---
+++ src/components/Tours/ToursLayout.tsx
@@ -0,0 +1,15 @@
+import toursHero from "../../assets/toursHero.jpg";
+import TourSearchBar from "../UI/TourSearchBar.tsx";
+
+export default function ToursPageLayout(){
+    return(
+        <div>
+            <div className="relative">
+                <img src={toursHero} className="h-[204px] w-full object-cover" alt="tour search area"></img>
+                <div className="absolute flex flex-row inset-1 items-center justify-center">
+                    <TourSearchBar />
+                </div>
+            </div>
+        </div>
+    )
+}
